80ee2044124e49b8ccfe31ec1e3d6570c84973aa
Connor <connorwilliamduke@gmail.com>
2021-07-13 19:17:23 +0100

    Added config file

---
+++ conf.lua
@@ -0,0 +1,9 @@
+function love.conf(t)
+  t.window.x = nil
+  t.window.y = nil
+  t.window.width = 1280
+  t.window.height = 720
+  t.window.title = "Game Window"
+  t.window.vsync = true
+  t.window.resizable = false
+end
